16b1bc0b327f470cf56df1cae2ba340c55fd15b0
Boris Parak <parak@cesnet.cz>
2017-03-31 14:21:55 +0200

    Version bump to cloudkeeper-one v1.1.2

--- config/projects/cloudkeeper-one.rb
+++ config/projects/cloudkeeper-one.rb
@@ -6,7 +6,7 @@ homepage 'https://github.com/the-cloudkeeper-project/cloudkeeper-one'
 description 'Synchronize cloud appliances between cloudkeeper and OpenNebula.'
 
 install_dir     '/opt/cloudkeeper-one'
-build_version   "1.1.1"
+build_version   "1.1.2"
 build_iteration 1
 
 override :rubygems, :version => '2.6.8'
