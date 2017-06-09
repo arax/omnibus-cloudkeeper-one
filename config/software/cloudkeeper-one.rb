fe13f26a2f0d89123423485b8b7115433294d0b8
Boris Parak <parak@cesnet.cz>
2017-06-09 09:54:52 +0200

    Version bump to cloudkeeper-one v1.2.4

--- config/software/cloudkeeper-one.rb
+++ config/software/cloudkeeper-one.rb
@@ -1,6 +1,6 @@
 name "cloudkeeper-one"
 ## WARN: do not forget to change CLOUDKEEPER_ONE_VERSION in the postinst script
-default_version "1.2.3"
+default_version "1.2.4"
 
 dependency "ruby"
 dependency "rubygems"
